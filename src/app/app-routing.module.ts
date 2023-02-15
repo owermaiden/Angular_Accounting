aec663cd6ce9c632e546f454b52ab065d09d24e5
owermaiden <ower.maiden@cybertekschool.com>
2023-02-15 13:39:08 +0100

    some

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoryListComponent } from './category/category-list/category-list.component';
 import { UserCreateComponent } from './user/user-create/user-create.component';
 import { UserUpdateComponent } from './user/user-update/user-update.component';
 import { UserlistComponent } from './user/userlist/userlist.component';
@@ -7,7 +8,8 @@ import { UserlistComponent } from './user/userlist/userlist.component';
 const routes: Routes = [
   { path: 'userlist', component: UserlistComponent },
   { path: 'user-update/:id', component: UserUpdateComponent },
-  { path: 'user-detail', component: UserCreateComponent }
+  { path: 'user-detail', component: UserCreateComponent },
+  { path: 'categories', component: CategoryListComponent}
  
 ];
 
